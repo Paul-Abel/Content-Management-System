3adbb0e9d7165b132b13ecb66b2f93c788398985
Paul Abel <abelpaul198@gmail.com>
2023-12-11 15:27:25 +0100

    Add a navigation from magazineList to magazine

--- src/main/java/view/MagazineBean.java
+++ src/main/java/view/MagazineBean.java
@@ -1,90 +1,42 @@
 package view;
 
-import entity.Magazine;
-import jakarta.annotation.PostConstruct;
-import jakarta.enterprise.context.SessionScoped;
-import jakarta.faces.application.FacesMessage;
-import jakarta.faces.context.FacesContext;
-import jakarta.faces.view.ViewScoped;
-import jakarta.inject.Inject;
-import jakarta.inject.Named;
 import bridge.MagazineBridge;
-import org.primefaces.PrimeFaces;
+import entity.Magazine;
+import jakarta.faces.annotation.ManagedProperty;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 @Named
 @ViewScoped
 public class MagazineBean implements Serializable {
 
     private MagazineBridge magazineBridge;
-    private List<Magazine> selectedMagazines = new ArrayList<>();
-    private Magazine selectedProduct;
-    private List<Magazine> allMagazines;
+    private Magazine magazine;
+    private Long id;
 
-    @PostConstruct
+
+
     public void init(){
         this.magazineBridge = new MagazineBridge();
-        this.allMagazines = magazineBridge.getAllMagazines();
-    }
-
-    public void updateAllMagazine(){
-        this.allMagazines = magazineBridge.getAllMagazines();
-    }
-    public List<Magazine> getAllMagazines(){
-        return this.allMagazines;
-    }
-    public List<Magazine> getSelectedMagazines() {
-        return selectedMagazines;
-    }
-    public void setSelectedMagazines(List<Magazine> selectedMagazines) {
-        this.selectedMagazines = selectedMagazines;
-    }
-    public Magazine getSelectedProduct() {
-        return selectedProduct;
-    }
-
-    public void setSelectedProduct(Magazine selectedProduct) {
-        this.selectedProduct = selectedProduct;
+        magazine = magazineBridge.getMagazine(id);
     }
 
 
-    public void addMagazine(){
-        Magazine magazine = new Magazine();
-        magazine.setTitle("Hallo");
-        magazine.setDescription("Welt");
-        this.magazineBridge.createMagazine(magazine);
-        updateAllMagazine();
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return magazine.getTitle();
+    }
+    public String getDescribtion() {
+        return magazine.getDescription();
     }
 
-    public void deleteMagazines(){
-        this.magazineBridge.deleteMultipleMagazines(selectedMagazines);
-        this.selectedMagazines = null;
-        this.selectedProduct=null;
-        updateAllMagazine();
-        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Magazine entfernt","Löschen war erfolgreich"));
-        PrimeFaces.current().ajax().update("form:messages", "form:allMagazineList");
-    }
-
-    public void deleteMagazine(Magazine magazine){
-        this.magazineBridge.deleteMagazine(magazine);
-        this.selectedProduct = null;
-        updateAllMagazine();
-        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Magazin entfernt","Löschen war erfolgreich"));
-        PrimeFaces.current().ajax().update("form:messages", "form:allMagazineList");
-    }
-
-    public boolean hasSelectedMagazines() {
-        return !this.selectedMagazines.isEmpty();
-    }
-
-    public String getDeleteButtonMessage() {
-        if (hasSelectedMagazines()) {
-            int size = this.selectedMagazines.size();
-            return size + " ausgewählt";
-        }
-        return "Löschen";
-    }
 }
